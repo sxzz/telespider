da11c5ebe165c2e218e3c35262413a26d5460fef
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-02 07:56:15 +0800

    chore: zero config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: 'esm',
-  target: 'node20.18',
-  clean: true,
-})
+export default defineConfig({})
